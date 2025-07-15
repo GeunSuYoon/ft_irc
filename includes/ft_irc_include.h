2a0fc501b8cf7cf8e78dd0d7e6bab6a4b267995a
Geunsu Yoon <geuyoon@cx2r1s2.42seoul.kr>
2025-07-15 18:11:19 +0900

    feat: Add includes header file

---
+++ includes/ft_irc_include.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_irc_include.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geuyoon <geuyoon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 21:37:35 by geuyoon           #+#    #+#             */
+/*   Updated: 2025/07/15 18:10:20 by geuyoon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_IRC_INCLUDE_H
+# define FT_IRC_INCLUDE_H
+
+# include <iostream>
+# include <sys/types.h>
+# include <sys/socket.h>
+# include <sys/epoll.h>
+# include <sys/stat.h>
+# include <netdb.h>
+# include <arpa/inet.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+#endif
